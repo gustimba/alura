e03b2bc8a42df269ad8a448bef760912114ae303
gustimba <gusta_tamy@hotmail.com>
2016-12-03 22:21:45 -0200

    C#

---
+++ Eleição/Eleição/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eleição
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade = 16;
+            bool brasileiro = true;
+            
+            if(idade >= 16  && brasileiro == true)
+            {
+                MessageBox.Show("pode Votar");
+
+            } else
+            {
+                MessageBox.Show("idade insuficiente ou Voce nao é brasileiro");
+            }
+
+        }
+    }
+}
